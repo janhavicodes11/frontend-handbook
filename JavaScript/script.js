ad02494db5f37f874849b7934fdce794d3584b48
janhavicodes11 <janhavi.hariom2026@gmail.com>
2026-04-01 12:06:56 +0530

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,114 @@
+// problems with var 
+//let a=10
+//let a=20 // thos shows an error because we cannot redeclare a variable with let keyword that will save us from errors
+// whereas in var you can redecale the variable and it will not show any error but it will override the previous value of a and assign the new value to a
+
+//var a =12  window main add hogya
+//let a=12 // window main add nahi hua 
+
+
+
+
+
+//Scope(aapka dayra kaha tak hai means aapka variable kaha tak access ho sakta hai)(types-globals, block, function)
+//var a=10 //this is global variable khole main declare kiya hai  and within no curly braces
+
+
+
+//block scope
+//{
+    //var b=20 ye block hai because within curly braces+ var  wont respect  block scope it only respect function scope so var kahi bhi ye use ho sakta hai
+    //let a=20 // ye respect karta hai block scope ki because ye khud hai ek block hai so yaani isse block main we can use this let 12 not anywhere else
+
+//}
+
+
+// functiional scoped-
+//function abcs (){
+    //var c=30 // this is function scoped variable yaani ye pure function main access ho sakta hai but function ke bahar access nahi ho sakta hai
+//}
+
+
+
+
+// Reassignment and redeclaration :- 
+//var a=10
+//a=20 // this is reassignment because we are changing the value of a from 10 to 20
+//var a=30 // this is redeclaration because we are declaring the variable a again and assigning it a new value of 30
+
+
+
+
+
+// Temporal Dead Zone (TDZ) :- utna area jitne mein js ko pata to hai ki variable exist karta hai par wo aapko value nahi de sakta
+//console.log(a)
+
+
+
+
+
+
+
+
+// this area between line 44 till 53 is tdz zone 
+//let a= 12;
+
+//Hosting impact per type -
+//console.log(a)
+//var a=10;
+
+
+
+
+//Data types :-
+// Primitive- string, number, boolean, null, undefined, symbol, bigInt 
+// primitive- values jinhe copy karne par real value milti hai
+//Reference- object, array, function- aise saare data types inko copy karne main real value nahi milti instead we get its parents value 
+
+
+//var a = 12;
+//var b = a; 
+//console.log(a);
+//console.log(b);
+
+//let a= [1,2,3,4]
+//let b = a;
+
+//single quote= ''
+//double quotes= '' ''
+//backticks= ``
+
+// null yaani aapne jaan bughkar value nahi di 
+// undefined yaani aapne variable banaya but usse value nahi di  so jo value bydefault mile 
+//let a; // this is undefined because we have not assigned any value to a so by default it will be undefines 
+//console.log(a);
+
+// symbol- uniue immnutable value 
+//let u1= Symbol('janhavi');
+
+//BigInt- this is used to store large integers that are beyond the range of the number data type
+
+//let a= 9007199254740991n;
+
+
+// Reference data types- changes are done in the reference value not in the real value
+//array 
+//let a=[1,2,3,4]
+//let b=a; 
+
+//object 
+//let a= {
+   // name: 'janhavi',
+//}
+//let b=a;
+
+
+//Dynamic type - Js main static typing nahi hai  that means aap data ko change kar saktey ho kyuki yaha par dynamic data types hai 
+let a= 12;
+a=true ;
+ console.log(a);
+
+ //since yaha ham state nahi kar rahe ki int hai bollean hai etc we can put any values
+
+ // type coercsion concept jismain aapka ek type automatically convert hojayega 
+ // "5"+1 will give 51 as yaha + will act as concetination
