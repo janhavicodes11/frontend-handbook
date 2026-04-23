9429ff2eb0ddcbfc384b58664984cb771dcfe7a9
Janhavi <janhavi.hariom2026@gmail.com>
2026-04-23 19:17:53 +0530

    Add files via upload

---
+++ JavaScript/Functions/script.js
@@ -0,0 +1,14 @@
+// what is function 
+// agar ham code likhe hai wo turant chal jata hai example
+// console.log("hey")
+// but we want button click karne pe wo code chale like ac not apne aap 
+
+// now using function :
+function dance(){
+    console.log("hey")
+    console.log("hey")
+    console.log("hey")
+    console.log("hey")
+
+}
+dance();
